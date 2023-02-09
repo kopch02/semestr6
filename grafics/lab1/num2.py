3d032e09abeb7a2b15270e68bedfc9decd3c3eae
Kopch02 <kopch08@gmail.com>
2023-02-09 14:41:00 +0700

    triangel

--- grafics/lab1/num2.py
+++ grafics/lab1/num2.py
@@ -1,83 +1,77 @@
 import tkinter
 import math
+import numpy as np
 
 a = 260
-h = int(a/2 * math.sqrt(3))
+h = int(a / 2 * math.sqrt(3))
 
 R = 150
 R2 = 75
 
-center = [130,75]
+center = [130, 75]
 
-print(h)
+tria_point = [[0, 0], [a, 0], [a / 2, h]]
+
 
 def triangel_del():
     canvas.delete(t)
 
 
 def triangel():
-    global canvas, t,h,a
-    t = canvas.create_polygon(0,
-                              0,
-                              a,
-                              0,
-                              a/2,
-                              h,
-                              0,
-                              0,
+    global canvas, t, h, a
+    canvas.delete(t)
+    t = canvas.create_polygon(tria_point[0][0],
+                              tria_point[0][1],
+                              tria_point[1][0],
+                              tria_point[1][1],
+                              tria_point[2][0],
+                              tria_point[2][1],
                               fill="white",
                               outline='black')
     canvas.move(t, 100, 100)
-    canvas.create_oval(center[0]+98,center[1]+98,center[0]+102,center[1]+102,fill = "green")
-
+    canvas.create_oval(center[0] + 98,
+                       center[1] + 98,
+                       center[0] + 102,
+                       center[1] + 102,
+                       fill="green")
 
 
 def left():
-    global canvas, t,h,a
+    global canvas, t, h, a, tria_point
+
+    alf = -np.pi / 4
 
     canvas.delete(t)
-    f = math.asin(175/R)
-    x1 = math.cos(f) * R
-    y1 = math.sin(f) * R
+    for i in range(3):
 
-    f = math.asin(175/R)
-    x2 = math.cos(f) * R
-    y2 = math.sin(f) * R
+        x = tria_point[i][0]
+        y = tria_point[i][1]
 
-    f = math.asin(325/R)
-    x3 = math.cos(f) * R
-    y3 = math.sin(f) * R
+        tria_point[i][0] = center[0] + (x - center[0]) * np.cos(alf) - (
+            y - center[1]) * np.sin(alf)
+        tria_point[i][1] = center[1] + (x - center[0]) * np.sin(alf) + (
+            y - center[1]) * np.cos(alf)
 
-    print(x1,y1)
-    print(x2,y2)
-    print(x3,y3)
-
-    t = canvas.create_polygon(0,
-                              0,
-                              a,
-                              0,
-                              a/2,
-                              h,
-                              0,
-                              0,
-                              fill="white",
-                              outline='black')
-    canvas.move(t, 100, 100)
+    triangel()
 
 
 def right():
+    global canvas, t, h, a, tria_point
+
+    alf = np.pi / 4
+
     canvas.delete(t)
-    t = canvas.create_polygon(0,
-                              0,
-                              a,
-                              0,
-                              a/2,
-                              h,
-                              0,
-                              0,
-                              fill="white",
-                              outline='black')
-    canvas.move(t, 100, 100)
+    for i in range(3):
+
+        x = tria_point[i][0]
+        y = tria_point[i][1]
+
+        tria_point[i][0] = center[0] + (x - center[0]) * np.cos(alf) - (
+            y - center[1]) * np.sin(alf)
+        tria_point[i][1] = center[1] + (x - center[0]) * np.sin(alf) + (
+            y - center[1]) * np.cos(alf)
+
+    triangel()
 
 
 t = tkinter.Tk()
@@ -93,9 +87,6 @@ b_right = tkinter.Button(t, text="->", command=right)
 
 b_left.place(x=50, y=490)
 b_right.place(x=80, y=490)
-
-
-
 
 b1.pack()
 bd1.pack()
