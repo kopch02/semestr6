1728eecbc9f94fc49e134c7f59e67d201f8187f6
Kopch02 <kopch08@gmail.com>
2023-03-28 23:19:50 +0700

    num2

--- mega_python/lab5/main.py
+++ mega_python/lab5/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, render_template, redirect, send_from_directory
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
@@ -12,6 +12,13 @@ def main():
     db_session.global_init("db/mars_explorer.db")
     app.run()
 
+@app.route('/')
+def index():
+    param = {}
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    param["jobs"] = jobs
+    return render_template('index.html', **param)
 
 if __name__ == '__main__':
     main()
